d333f58df345059fcbb8b188b008bd2d083db2cf
zigaass <ziga.stare88@gmail.com>
2026-09-03 09:59:32 +0000

    Add multiplication function to calculate and display multiplication table

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -5,6 +5,14 @@ def hello():
     else:
         print(f"Hello {oddelek}")
 
+def poštevanka():
+    x = int(input("izberi si število")) 
+    št=1
+    while št<=10:
+        print(f"{št}*{x}={št*x}")
+        št+=1
+
+
 if __name__ == "__main__":
     hello()
 
